4d34ab924de114e62998d4cf8e54f3aadd91ad38
Json449 <Json58016@gmail.com>
2025-04-13 16:37:38 +0500

    added backend prefix

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.setGlobalPrefix('backend');
     app.use(cookieParser());
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -17,7 +18,7 @@ async function bootstrap() {
     }));
     const port = process.env.PORT || 3000;
     await app.listen(port);
-    console.log(`Application is running on: http://localhost:${port}`);
+    console.log(`Application is running on: http://localhost:${port}/backend`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
