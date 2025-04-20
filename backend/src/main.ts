544c6168279786a7eac8524441aac35304cb1571
Json449 <Json58016@gmail.com>
2025-04-21 00:03:21 +0500

    fixed issues

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}/backend`);
+  console.log(`Application is running on: http://localhost:${port}/api`);
 }
 
 bootstrap();
